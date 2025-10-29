d94fc0aa727cdf7adcc1a05d3a1a032962945640
Christophe Tav <christophe.tav@gmail.com>
2025-10-29 10:49:49 -0400

    add here library

--- scripts/generate_hex_sticker_logo/02_generate_hex_sticker.R
+++ scripts/generate_hex_sticker_logo/02_generate_hex_sticker.R
@@ -1,12 +1,14 @@
 library(hexSticker) # devtools::install_github("GuangchuangYu/hexSticker")
 library(showtext)
+library(here)
 
 font_add("Century Gothic", regular = here("input", "fonts", "centurygothic.ttf"))
 showtext_auto()  # enable showtext
 
 imgurl <- here("output", "hex_sticker_logo", "venn_logo.png")
 
-sticker(imgurl, package="gVenn",
+# png
+sticker(imgurl, package = "gVenn",
         p_x = 1, p_y = 0.45, # position for package name
         s_x = 1, s_y = 1.2, # position for subplot
         s_width = 0.55, # size for subplot
