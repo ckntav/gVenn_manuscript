b4264307b5ac7a88c8e602a33aef9f24fe46ca2d
Christophe Tav <christophe.tav@gmail.com>
2025-11-01 14:58:32 -0400

    add sticker with url

--- scripts/generate_hex_sticker_logo/02_generate_hex_sticker.R
+++ scripts/generate_hex_sticker_logo/02_generate_hex_sticker.R
@@ -17,3 +17,16 @@ sticker(imgurl, package = "gVenn",
         h_size = 1.2, h_color = "black",
         dpi = 600, , p_color = "black",
         filename = here("output", "hex_sticker_logo", "gVenn_hex_sticker.png"))
+
+# png with url
+sticker(imgurl, package = "gVenn",
+        p_x = 1, p_y = 0.45, # position for package name
+        s_x = 1, s_y = 1.2, # position for subplot
+        s_width = 0.55, # size for subplot
+        p_family = "Century Gothic", p_size = 36, # font for package name
+        h_fill = "white",
+        h_size = 1.2, h_color = "black",
+        dpi = 600, , p_color = "black",
+        url = "github.com/ckntav/gVenn",
+        u_color = "#303030", u_size = 5, u_family = "Century Gothic",
+        filename = here("output", "hex_sticker_logo", "gVenn_hex_sticker_with_url.png"))
