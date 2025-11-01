12bad0deddbb71b1fcdd36d6449a6dbbdfbc7891
Christophe Tav <christophe.tav@gmail.com>
2025-11-01 14:51:01 -0400

    update

--- scripts/create_figure1/03_build_figure1BC.R
+++ scripts/create_figure1/03_build_figure1BC.R
@@ -1,13 +1,12 @@
-setwd("/Users/chris/Desktop/20251013_A549_gVenn")
-
 # Load required packages
 library(gVenn)
-library(rtracklayer)
+library(GenomicRanges)
+library(here)
 
 # Import the demonstration BED files
-peakset1 <- rtracklayer::import("output/mock_bed/peakset1.bed")
-peakset2 <- rtracklayer::import("output/mock_bed/peakset2.bed")
-peakset3 <- rtracklayer::import("output/mock_bed/peakset3.bed")
+peakset1 <- rtracklayer::import(here("input", "example_bed", "peakset1.bed"))
+peakset2 <- rtracklayer::import(here("input", "example_bed", "peakset2.bed"))
+peakset3 <- rtracklayer::import(here("input", "example_bed", "peakset3.bed"))
 
 # Combine into a GRangesList
 demo_peaks <- GRangesList("peakset1" = peakset1,
@@ -20,36 +19,21 @@ sapply(demo_peaks, length)
 ov <- computeOverlaps(demo_peaks)
 
 # Visualize with Venn diagram
-venn <-
-plotVenn(ov)
+venn <- plotVenn(ov)
+venn
 
 saveViz(venn,
-        output_dir = "output/analysis/dataviz",
-        output_file = "example_venn",
+        output_dir = here("output", "figure1"),
+        output_file = "fig1B_example_venn",
+        with_date = FALSE,
         width = 4, height = 2.5)
 
 # Visualize with UpSet plot (better for 3+ sets)
-upset <-
-plotUpSet(ov,
-          comb_col = c( "#D87093",  "#CD3301", "#9370DB", "#008B8B", "#2B70AB", "#FFB027", "#3EA742"))
+upset <- plotUpSet(ov,
+                   comb_col = c( "#D87093",  "#CD3301", "#9370DB", "#008B8B", "#2B70AB", "#FFB027", "#3EA742"))
 
 saveViz(upset,
-        output_dir = "output/analysis/dataviz",
-        output_file = "example_upset",
+        output_dir = here("output", "figure1"),
+        output_file = "fig1C_example_upset",
+        with_date = FALSE,
         width = 5, height = 3)
-
-# Get reduced regions (merged overlapping areas)
-demo_reduced_regions <- ov$reduced_regions
-cat("Total reduced regions:", length(demo_reduced_regions), "\n")
-rtracklayer::export(demo_reduced_regions, format = "bed", con = "output/mock_bed/example_reduced_regions.bed")
-
-# Extract all overlap groups
-overlapGroups <- extractOverlaps(ov)
-cat("\nOverlap group sizes:\n")
-sapply(overlapGroups, length)
-
-#
-exportOverlapsToBed(overlapGroups,
-                    output_dir = "output/mock_bed",
-                    output_prefix = "example",
-                    with_date = FALSE)
